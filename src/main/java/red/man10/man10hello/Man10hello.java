b2fe538d21b9a07c893620ad2cd2719aee978883
BuildTools <unconfigured@null.spigotmc.org>
2017-02-27 12:48:19 +0900

    uuid save method update

--- src/main/java/red/man10/man10hello/Man10hello.java
+++ src/main/java/red/man10/man10hello/Man10hello.java
@@ -9,6 +9,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -18,6 +19,10 @@ public final class Man10hello extends JavaPlugin implements Listener {
     double tx;
     double ty;
     double tz;
+
+    double vx;
+    double vy;
+    double vz;
 
     boolean enablebroad;
     boolean enableprefix;
@@ -77,15 +82,12 @@ public final class Man10hello extends JavaPlugin implements Listener {
     public void writePlayer(String pn){
         try {
             File file = new File("plugins/Man10hello/player.txt");
-            FileWriter filewriter = new FileWriter(file);
-            BufferedWriter bw = new BufferedWriter(filewriter);
-            bw.write(pn);
-            bw.write("\n");
-            bw.close();
-            configReload();
+            FileWriter filewriter = new FileWriter(file, true);
+            filewriter.write(pn + "\n");
+            filewriter.close();
             loadPlayerConfig();
         }catch (IOException e){
-
+            e.printStackTrace();
         }
     }
 
@@ -104,6 +106,12 @@ public final class Man10hello extends JavaPlugin implements Listener {
                         this.getConfig().set("tp.x", p.getLocation().getX());
                         this.getConfig().set("tp.y", p.getLocation().getY());
                         this.getConfig().set("tp.z", p.getLocation().getZ());
+
+                        Vector v = p.getLocation().getDirection();
+
+                        this.getConfig().set("tp.vx", v.getX());
+                        this.getConfig().set("tp.vy", v.getY());
+                        this.getConfig().set("tp.vz", v.getZ());
                         this.saveConfig();
                         p.sendMessage(prefix + "§a§lTPロケーションを設定しました。");
                         reloadConfig();
@@ -135,6 +143,10 @@ public final class Man10hello extends JavaPlugin implements Listener {
         ty = this.getConfig().getDouble("tp.y");
         tz = this.getConfig().getDouble("tp.z");
 
+        vx = this.getConfig().getDouble("tp.vx");
+        vy = this.getConfig().getDouble("tp.vy");
+        vz = this.getConfig().getDouble("tp.vz");
+
         enablebroad = this.getConfig().getBoolean("message.enable_broadcast");
         enableprefix = this.getConfig().getBoolean("message.enable_prefix");
 
@@ -146,7 +158,6 @@ public final class Man10hello extends JavaPlugin implements Listener {
             text.add(s);
         }
 
-
     }
 
     @EventHandler
@@ -154,23 +165,27 @@ public final class Man10hello extends JavaPlugin implements Listener {
         Player p = e.getPlayer();
         for(String t : text) {
             if (e.getMessage().contains(t)) {
-                if (playername.contains(p.getName())) {
-
+                if (playername.contains(p.getUniqueId().toString())) {
                 } else {
+                    Vector v = new Vector(vx,vy,vz);
                     Location l = new Location(p.getWorld(), tx, ty, tz);
+
                     if (enablebroad == true) {
                         if (enableprefix == true) {
                             Bukkit.getServer().broadcastMessage(prefix + bmessage.replaceAll("%PLAYER%", p.getName()));
                             p.teleport(l);
+                            p.getLocation().setDirection(v);
 
                         } else {
                             Bukkit.getServer().broadcastMessage(bmessage.replaceAll("%PLAYER%", p.getName()));
                             p.teleport(l);
+                            p.getLocation().setDirection(v);
                         }
                     } else {
                         p.teleport(l);
+                        p.getLocation().setDirection(v);
                     }
-                    writePlayer(p.getName());
+                    writePlayer(p.getUniqueId().toString());
                 }
             }
         }
